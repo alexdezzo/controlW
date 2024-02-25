72a2a12c21fd510f3d7484a1bdb59bee9e5c0aef
Alexander Gonchar <alexandergonchar301@gmail.com>
2024-02-25 19:35:10 +0500

    Declaring an array. Adding items via the console

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Задайте размер массива:");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+//Объявление массива и занесение элементов через консоль
+string[] array = new string[sizeArray];
+
+Console.WriteLine($"Введите {sizeArray} значеня(-ий) в массив");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите строку ({i + 1}/{sizeArray})");
+    array[i] = Console.ReadLine();
+}
